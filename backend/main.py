5128b3412f2ef3b03e9bc4443689ee66162ae72f
mrizqihidayat <m.rizqi.h.aja@gmail.com>
2026-07-29 17:53:02 +0700

    fix cors

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 from app import create_app
+from flask_cors import CORS 
 
 app = create_app()
+
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=app.config["DEBUG"])
